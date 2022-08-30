340b7ef8018fa532646b7e4e8ee659e526c9966f
jomunlak <xofla002@gmail.com>
2022-08-30 19:18:00 +0900

    useradmin에서 room을 확인할 수 있는 기능 추가

--- users/admin.py
+++ users/admin.py
@@ -1,16 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from . import models
-
+from rooms import models as room_models
 
 # Register your models here.
 # admin 패널에 반영되는 내용
+
+
+class RoomInline(admin.TabularInline):
+    model = room_models.Room
 
 
 @admin.register(models.User)
 class CustomUserAdmin(UserAdmin):
 
     """Custom User Admin"""
+
+    inlines = [
+        RoomInline,
+    ]
 
     list_display = (
         "username",
