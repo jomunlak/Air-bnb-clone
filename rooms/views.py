c4898c5c3aaa681d56c6d99843b0499096a53f49
jomunlak <xofla002@gmail.com>
2022-08-31 19:04:57 +0900

    added templates

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,11 @@
+from datetime import datetime
 from django.shortcuts import render
+from django.http import HttpResponse
+from . import models as room_models
 
 # Create your views here.
+
+
+def all_rooms(request):
+    all_rooms = room_models.Room.objects.all()
+    return render(request, "rooms/all_rooms.html", context={"rooms": all_rooms})
