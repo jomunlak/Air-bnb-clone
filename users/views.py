f59e2d3e476d0044ce14f2bd78ba93439a55e1d5
jomunlak <xofla002@gmail.com>
2022-09-08 02:12:37 +0900

    login and logout

--- users/views.py
+++ users/views.py
@@ -1,4 +1,45 @@
-from django.shortcuts import render
+from audioop import reverse
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
+from django.views import View
+from django.views.generic import FormView
+from django.contrib.auth import authenticate, login, logout
+from . import forms
 
 # Create your views here.
 # render the html
+
+
+class LoginView(FormView):
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+
+    # def get(self, request):
+    #     form = forms.LoginForm(initial={"email": "xofla002@gmail.com"})
+    #     return render(request, "users/login.html", {"form": form})
+
+    # def post(self, request):
+    #     form = forms.LoginForm(request.POST)
+
+    #     if form.is_valid():
+    #         email = form.cleaned_data.get("email")
+    #         password = form.cleaned_data.get("password")
+    #         user = authenticate(request, username=email, password=password)
+    #         if user is not None:
+    #             login(request, user)
+    #             return redirect(reverse("core:home"))
+    #     return render(request, "users/login.html", {"form": form})
+
+
+def LogoutView(request):
+    logout(request)
+    return redirect(reverse("core:home"))
